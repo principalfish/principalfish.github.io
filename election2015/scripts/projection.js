00a3300ff1e7b2b49c6e8725de46a69cfe0ed34e
principalfish <philipfharrison@gmail.com>
2015-02-04 12:57:34 +0000

    projectiontest

--- election2015/scripts/projection.js
+++ election2015/scripts/projection.js
@@ -3,7 +3,7 @@ d3.selection.prototype.moveToFront = function() {
 							return this.each(function(){
 							this.parentNode.appendChild(this);
 						});
-					}; 	
+					};
 
 // far left filter scripts
 var filterStates = [{party: "null"}, {gain:"null"}, {region: "null"}]
@@ -13,49 +13,49 @@ var searchSeatData = [];
 var seatNames = [];
 
 
-	
+
 
 function filterMap(){
 
 	var	party  = filterStates[0].party;
 	var gains = filterStates[1].gain;
 	var region = filterStates[2].region;
-	
+
 	seatsAfterFilter = [];
-		
-	d3.json("/election2015/data/map.json", function(uk){
-		
+
+	d3.json("/election2015/data/projection.json", function(uk){
+
 		if (party == "null" && region == "null" && gains == "null")
 			g.selectAll(".map")
 				.attr("style", "opacity:1")
-						
+
 		else
-		
-				
+
+
 			g.selectAll(".map")
 				.attr("id", "filtertime")
-				
+
 			if (party == "null")
 				g.selectAll("#filtertime")
 					.attr("id", "partyfiltered")
 			else
-				g.selectAll("#filtertime")					
+				g.selectAll("#filtertime")
 					.attr("style", function(d){
 						if (party != d.properties.info_party)
 							return "opacity: 0.1";
-						})					
+						})
 					.attr("id", function(d){
 						if (party == d.properties.info_party)
-							return "partyfiltered"	
+							return "partyfiltered"
 						});
-											
-				
-			
-		
+
+
+
+
 			if (gains == "null")
 					g.selectAll("#partyfiltered")
 						.attr("id", "gainfiltered")
-			
+
 			else
 				if (gains == "gains")
 					g.selectAll("#partyfiltered")
@@ -67,7 +67,7 @@ function filterMap(){
 							if (d.properties.info_party != d.properties.info_incumbent)
 								return "gainfiltered"
 						});
-				
+
 				if (gains == "nochange")
 					g.selectAll("#partyfiltered")
 						.attr("style", function(d) {
@@ -78,34 +78,34 @@ function filterMap(){
 							if (d.properties.info_party == d.properties.info_incumbent)
 								return "gainfiltered"
 						});
-			
-		
-			
+
+
+
 			if (region == "null")
 				g.selectAll("#gainfiltered")
 					.each(function(d){
-							seatsAfterFilter.push(d)					
-												
+							seatsAfterFilter.push(d)
+
 						})
 			else
-				g.selectAll("#gainfiltered")	
-					.attr("style", function(d){																		
-						if (region != d.properties.info_area)								
-							return "opacity: 0.1";	
+				g.selectAll("#gainfiltered")
+					.attr("style", function(d){
+						if (region != d.properties.info_area)
+							return "opacity: 0.1";
 					})
-					.attr("id", function(d){																		
-						if (region == d.properties.info_area)			
-							seatsAfterFilter.push(d);												
-					});	
-					
+					.attr("id", function(d){
+						if (region == d.properties.info_area)
+							seatsAfterFilter.push(d);
+					});
+
 			g.selectAll(".map")
 				.attr("id", function(d) {
 					return "i" + d.properties.info_id
 				});
 			$("#totalfilteredseats").html(" ");
 			$("#filteredlisttable").html(" ");
-			
-	
+
+
 		});
 	}
 
@@ -113,9 +113,9 @@ function resetFilter(){
 
 	filterStates[0].party = "null"
 	filterStates[1].gain = "null"
-	filterStates[2].region = "null"							
+	filterStates[2].region = "null"
 	filterMap();
-	
+
 	$("#dropdownparty option:eq(0)").prop("selected", true);
 	$("#dropdowngains option:eq(0)").prop("selected", true);
 	$("#dropdownregion option:eq(0)").prop("selected", true);
@@ -124,39 +124,39 @@ function resetFilter(){
 	$("#filteredlisttable").html(" ");
 }
 
-function generateSeatList(){			
+function generateSeatList(){
 			$("#totalfilteredseats").html(" ");
 			$("#filteredlisttable").html(" ");
-			
+
 			$("#totalfilteredseats").append("<p>Total : " + seatsAfterFilter.length + "</p>");
 			$.each(seatsAfterFilter, function(i){
-				$("#filteredlisttable").append("<tr class=" + seatsAfterFilter[i].properties.info_party + 
+				$("#filteredlisttable").append("<tr class=" + seatsAfterFilter[i].properties.info_party +
 				"><td onclick=\"zoomToClickedFilteredSeat(seatsAfterFilter[" + i + "])\">" + seatsAfterFilter[i].properties.name + "</td></tr>")
-				
+
 			});
-				
-			
-		}	
-		
+
+
+		}
+
 function zoomToClickedFilteredSeat(d){
-	var id = "#i" + d.properties.info_id;	
+	var id = "#i" + d.properties.info_id;
 	//rewrite at some point
-	
+
 	previous = d3.select(previousnode)
 	current = d3.select(id);
-	
+
 	repeat();
-	
+
 	function repeat(){
 		previous.transition()
 			.attr("opacity", 1)
-		
-		current		
-			.transition()			
-				.duration(1500)		
+
+		current
+			.transition()
+				.duration(1500)
 				.attr("opacity", 0.2)
-			.transition()		
-				.duration(1500)		
+			.transition()
+				.duration(1500)
 				.attr("opacity", 1)
 			.each("end", repeat);
 		}
@@ -165,18 +165,18 @@ function zoomToClickedFilteredSeat(d){
 		dx = Math.pow((bounds[1][0] - bounds[0][0]), 0.5),
 		dy = Math.pow((bounds[1][1] - bounds[0][1]), 0.5),
 		x = (bounds[0][0] + bounds[1][0]) / 2,
-		y = (bounds[0][1] + bounds[1][1]) / 2,		
-		
+		y = (bounds[0][1] + bounds[1][1]) / 2,
+
 		scale = .025 / Math.max(dx /  width,  dy / height),
 		translate = [width / 2 - scale * x, height / 2 - scale * y];
-		
+
 	disableZoom();
-		
-	svg.transition()		
-			.duration(1500)		
+
+	svg.transition()
+			.duration(1500)
 			.call(zoom.translate(translate).scale(scale).event)
 			.each("end", enableZoom);
-			
+
 	seatinfo(d);
 	previousnode = id;
 	}
@@ -184,7 +184,7 @@ function zoomToClickedFilteredSeat(d){
 
 
 
-// scripts for zoom/pan/clicked 
+// scripts for zoom/pan/clicked
 
 
 
@@ -192,19 +192,19 @@ function clicked(d) {
 
 	previous = d3.select(previousnode)
 	current = d3.select(this);
-	
+
 	repeat();
-	
+
 	function repeat(){
 		previous.transition()
 			.attr("opacity", 1)
-		
-		current		
-			.transition()			
-				.duration(1500)		
+
+		current
+			.transition()
+				.duration(1500)
 				.attr("opacity", 0.2)
-			.transition()		
-				.duration(1500)		
+			.transition()
+				.duration(1500)
 				.attr("opacity", 1)
 			.each("end", repeat);
 		}
@@ -213,18 +213,18 @@ function clicked(d) {
 		dx = Math.pow((bounds[1][0] - bounds[0][0]), 0.5),
 		dy = Math.pow((bounds[1][1] - bounds[0][1]), 0.5),
 		x = (bounds[0][0] + bounds[1][0]) / 2,
-		y = (bounds[0][1] + bounds[1][1]) / 2,		
-		
+		y = (bounds[0][1] + bounds[1][1]) / 2,
+
 		scale = .025 / Math.max(dx /  width,  dy / height),
 		translate = [width / 2 - scale * x, height / 2 - scale * y];
-		
+
 	disableZoom();
-		
-	svg.transition()		
-			.duration(1500)		
+
+	svg.transition()
+			.duration(1500)
 			.call(zoom.translate(translate).scale(scale).event)
 			.each("end", enableZoom);
-			
+
 	seatinfo(d);
 	previousnode = this;
 }
@@ -249,13 +249,13 @@ function reset() {
 		.duration(1500)
 		.call(zoom.translate([0, 0]).scale(1).event)
 		.each("end", enableZoom);
-		
+
 }
 
-function zoomed() {	
+function zoomed() {
 	g.style("stroke-width", 1.5 / d3.event.scale + "px");
 	g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-	
+
 }
 
 function stopped() {
@@ -267,84 +267,84 @@ function stopped() {
 var oldclass = null;
 
 function seatinfo(d){
-	
-	$("#information").removeClass(oldclass);		
-	$("#information").addClass(d.properties.info_party)			
-	$("#information-seatname").html("<td>Seat</td><td style=\"width:380px\"> " + d.properties.name + 
-	"</td><td id=\"rightcolumninfotable\">" + regionlist[d.properties.info_area] + "</td>");				
+
+	$("#information").removeClass(oldclass);
+	$("#information").addClass(d.properties.info_party)
+	$("#information-seatname").html("<td>Seat</td><td style=\"width:380px\"> " + d.properties.name +
+	"</td><td id=\"rightcolumninfotable\">" + regionlist[d.properties.info_area] + "</td>");
 	$("#information-party").html("<td>Party</td><td>" + partylist[d.properties.info_party] + "</td>");
 	if (d.properties.info_party != d.properties.info_incumbent)
-		$("#information-gain").html("<td>Gain from</td><td><span id=\"information-gain-span\"class=\"" + d.properties.info_incumbent + "\">" + partylist[d.properties.info_incumbent] + "</span></td>")			
+		$("#information-gain").html("<td>Gain from</td><td><span id=\"information-gain-span\"class=\"" + d.properties.info_incumbent + "\">" + partylist[d.properties.info_incumbent] + "</span></td>")
 	else
 		$("#information-gain").html("<td>No change </td>")
 	$("#information-pie").html(piechart(d));
-	
+
 	oldclass = d.properties.info_party;
 }
 
 
 
 function piechart(d){
-	
+
 	$("#information-pie").empty();
 	$("#information-chart").empty();
-	
+
 	$("#information-chart").html("<p></p>");
 
 	var data = [];
-	data.push({ party: "labour", votes: d.properties.info_LAB});
-	data.push({ party: "conservative", votes: d.properties.info_CON});
-	data.push({ party: "libdems", votes: d.properties.info_LIB});
-	data.push({ party: "ukip", votes: d.properties.info_UKIP});
-	data.push({ party: "snp", votes: d.properties.info_SNP});
-	data.push({ party: "plaidcymru", votes: d.properties.info_PC});
-	data.push({ party: "green", votes: d.properties.info_GRN});
-	data.push({ party: "dup", votes: d.properties.info_DUP});
-	data.push({ party: "sdlp", votes: d.properties.info_SDLP});
-	data.push({ party: "uu", votes: d.properties.info_UU});
-	data.push({ party: "sinnfein", votes: d.properties.info_SF});
-	data.push({ party: "alliance", votes: d.properties.info_ALL});
-	data.push({ party: "other", votes: d.properties.info_OTH});
+	data.push({ party: "labour", votes: d.properties.info_labour});
+	data.push({ party: "conservative", votes: d.properties.info_conservative});
+	data.push({ party: "libdems", votes: d.properties.info_libdems});
+	data.push({ party: "ukip", votes: d.properties.info_ukip});
+	data.push({ party: "snp", votes: d.properties.info_snp});
+	data.push({ party: "plaidcymru", votes: d.properties.info_plaidcymru});
+	data.push({ party: "green", votes: d.properties.info_green});
+	data.push({ party: "dup", votes: d.properties.info_dup});
+	data.push({ party: "sdlp", votes: d.properties.info_sdlp});
+	data.push({ party: "uu", votes: d.properties.info_uu});
+	data.push({ party: "sinnfein", votes: d.properties.info_sinnfein});
+	data.push({ party: "alliance", votes: d.properties.info_alliance});
+	data.push({ party: "other", votes: d.properties.info_other});
 
-	
+
 
 	var filterdata = [];
-	
-	
-	$.each(data, function(i){	
-		
-		if (data[i].votes > 0 && data[i].party != "other2")			
+
+
+	$.each(data, function(i){
+
+		if (data[i].votes > 0 && data[i].party != "other")
 			filterdata.push(data[i]);
 	});
-	
 
-	
-	filterdata.sort(function(a, b){						
-			return b.votes - a.votes ; 				
+
+
+	filterdata.sort(function(a, b){
+			return b.votes - a.votes ;
 	});
-	
+
 	var sumfilterdata = 0;
-	
+
 	$.each(filterdata, function(i) {sumfilterdata += filterdata[i].votes;})
-	
+
 	// better way of doing this?
-	
-	
+
+
 	var width = 250,
 		height = 250;
 		radius = Math.min(width, height) / 2;
-		
-								
+
+
 	var arc = d3.svg.arc()
 		.outerRadius(radius - 10)
-		.innerRadius(0);							
+		.innerRadius(0);
 
 	var pie = d3.layout.pie()
 		.sort(null)
 		.value(function (d) {
 			return d.votes;
 		});
-		
+
 	var svg = d3.select("#information-pie").append("svg")
 		.attr("width", width)
 		.attr("height", height)
@@ -357,25 +357,25 @@ function piechart(d){
 			.attr("class", "arc");
 
 		g.append("path")
-			.attr("d", arc)								
-			.attr("class", function(d, i){									
-					return filterdata[i].party;									
-				});		
-	
+			.attr("d", arc)
+			.attr("class", function(d, i){
+					return filterdata[i].party;
+				});
+
 	// creates table with vote counts/percentages
-	$.each(filterdata, function(i){							
-		$("#information-chart").append("<tr class=" + filterdata[i].party + " style=\"font-weight: bold;\"><td>" +  
+	$.each(filterdata, function(i){
+		$("#information-chart").append("<tr class=" + filterdata[i].party + " style=\"font-weight: bold;\"><td>" +
 			partylist[filterdata[i].party] + "</td><td>" + (100 * filterdata[i].votes/sumfilterdata).toFixed(2) +  "%</td></tr>")
 	})
-	
+
 }
 
 
-	
-// bottom of right - vote chart 
+
+// bottom of right - vote chart
 
 
-$( function() { 
+$( function() {
     $.tablesorter.addParser({
         id: "numberWithComma",
         is: function(s) {
@@ -388,34 +388,34 @@ $( function() {
     });
 });
 
-function doStuff(data) {		
+function doStuff(data) {
 		$.each(data, function(i){
 			if (i == data.length -1)
-				$("#totalstable").append("<tfoot><tr class=\"" + data[i].code +"\"><td>" + data[i].party + "</td><td>" 
+				$("#totalstable").append("<tfoot><tr class=\"" + data[i].code +"\"><td>" + data[i].party + "</td><td>"
 					+ data[i].seats + "</td><td>" + data[i].change + "</td><td>" + (data[i].votepercent).toFixed(2) + "</td></tr></tfoot>")
 			else
-				$("#totalstable").append("<tr class=\"" + data[i].code +"\"><td>" + data[i].party + "</td><td>" 
+				$("#totalstable").append("<tr class=\"" + data[i].code +"\"><td>" + data[i].party + "</td><td>"
 					+ data[i].seats + "</td><td>" + data[i].change + "</td><td>" + (data[i].votepercent).toFixed(2) + "</td></tr>")
-	
+
 		})
-		
-	 $(document).ready(function() { 
+
+	 $(document).ready(function() {
 		$("table").tablesorter({
-			headers: {			
-				0: { 
+			headers: {
+				0: {
 					sorter: false
-				}					
+				}
 			},
 			sortList:[[3,1], [1,0], [2,0]]
-			
-		}); 
+
+		});
 	});
 };
 
 function parseData(url, callBack) {
 	Papa.parse(url, {
 		download: true,
-		header: true,								
+		header: true,
 		dynamicTyping: true,
 		complete: function(results) {
 			callBack(results.data);
@@ -424,9 +424,9 @@ function parseData(url, callBack) {
 }
 
 parseData("/election2015/data/projectionvotetotals.csv", doStuff);
-	
-	
-// autocomplete 
+
+
+// autocomplete
 
 function searchSeats(value){
 	$.each(searchSeatData, function(i){
@@ -438,12 +438,12 @@ function searchSeats(value){
 $(function()
 {
 
-	$("#searchseats").autocomplete({	
+	$("#searchseats").autocomplete({
 		source: seatNames,
 		select: function(event, ui){
 			searchSeats(ui.item.label);
 		}
-	
+
 	});
-		
+
 });

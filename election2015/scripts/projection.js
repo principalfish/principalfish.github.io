293fed86c861607525c3066f8f572826b4a2b6a5
principalfish <philipfharrison@gmail.com>
2015-02-18 10:47:02 +0000

    more button crap

--- election2015/scripts/projection.js
+++ election2015/scripts/projection.js
@@ -525,3 +525,13 @@ $(function()
 	});
 
 });
+
+// about button
+
+function toggle_visibility(id){
+	var e = document.getElementById(id);
+	if (e.style.display == "block")
+		e.style.display = "none";
+	else
+		e.style.display = "block";
+}

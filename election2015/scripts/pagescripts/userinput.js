47ea73966d2cf05a94fc1993e4c13eb8fabd4825
principalfish <philipfharrison@gmail.com>
2015-03-01 16:50:02 +0000

    other change

---
+++ election2015/scripts/pagescripts/userinput.js
@@ -0,0 +1,27 @@
+// func to check user input percentages dont add up to more than 100 and to work out other percentage
+
+function userInputCheck(inputform, country){
+  var $form = $(inputform)
+  $sumpercentages = $form.find(".inputnumbers");
+
+  var sum = 0
+  $sumpercentages.each(function() {
+    var value = Number($(this).val());
+    if (!isNaN(value)) sum += value;
+  });
+
+  if (sum >= 100){
+    otherPercentage = "< 0"
+    alert("Percentages add up to more than 100!");
+  }
+
+
+
+  else
+    otherPercentage = 100 - sum
+
+  spanId = "#other" + country
+
+  $(spanId).html(otherPercentage);
+
+}

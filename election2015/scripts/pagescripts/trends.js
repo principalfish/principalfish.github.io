f6ac281b6ac7ba9bd1008446e13fd374203622af
principalfish <philipfharrison@gmail.com>
2015-03-11 11:47:38 +0000

    test trends

---
+++ election2015/scripts/pagescripts/trends.js
@@ -0,0 +1,65 @@
+var margin = {top: 30, right: 20, bottom: 30, left: 50},
+    width = 1280 - margin.left - margin.right,
+    height = 900 - margin.top - margin.bottom;
+
+// Parse the date / time
+var parseDate = d3.time.format("%Y-%m-%d").parse;
+
+
+// Set the ranges
+
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
+
+// Define the axes
+var xAxis = d3.svg.axis().scale(x)
+  .orient("bottom").ticks(5);
+
+var yAxis = d3.svg.axis().scale(y)
+  .orient("left").ticks(5);
+
+
+// Define the line
+var priceline = d3.svg.line()
+    .x(function(d) { return x(d.day); })
+    .y(function(d) { return y(d.seats); });
+
+
+
+// Get the data
+d3.tsv("/election2015/data/trends.csv", function(error, data) {
+    data.forEach(function(d) {
+    d.day = parseDate(d.day);
+    d.seats = +d.seats;
+    });
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.day; }));
+    y.domain([0, d3.max(data, function(d) { return d.seats; })]);
+
+    // Nest the entries by symbol
+    var dataNest = d3.nest()
+        .key(function(d) {return d.party;})
+        .entries(data);
+
+    // Loop through each symbol / key
+    dataNest.forEach(function(d) {
+
+        svg.append("path")
+            .attr("class", "line")
+            .attr("d", priceline(d.values));
+
+    });
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+});

f7559e55254364334448c3f55afe6cad90eeb931
principalfish <philipfharrison@gmail.com>
2015-03-11 12:49:44 +0000

    more trends

--- election2015/scripts/pagescripts/trends.js
+++ election2015/scripts/pagescripts/trends.js
@@ -24,18 +24,21 @@ var priceline = d3.svg.line()
     .x(function(d) { return x(d.day); })
     .y(function(d) { return y(d.seats); });
 
-
+var graphparties = ["conservative", "labour", "libdems", "ukip", "snp", "plaidcymru", "green"]
 
 // Get the data
 d3.tsv("/election2015/data/trends.csv", function(error, data) {
+
+
     data.forEach(function(d) {
-    d.day = parseDate(d.day);
-    d.seats = +d.seats;
+        d.day = parseDate(d.day);
+        d.seats = +d.seats;
+
     });
 
     // Scale the range of the data
     x.domain(d3.extent(data, function(d) { return d.day; }));
-    y.domain([0, d3.max(data, function(d) { return d.seats; })]);
+    y.domain([0, d3.max(data, function(d) { return d.seats; }) + 25]);
 
     // Nest the entries by symbol
     var dataNest = d3.nest()
@@ -44,16 +47,19 @@ d3.tsv("/election2015/data/trends.csv", function(error, data) {
 
     // Loop through each symbol / key
     dataNest.forEach(function(d) {
-
+      if (graphparties.indexOf(d.key) >= 0){
         svg.append("path")
-            .attr("class", "line")
-            .attr("d", priceline(d.values));
 
+          .attr("class", d.key)
+          .style("fill", "none")
+          .attr("d", priceline(d.values))
+        }
     });
 
     // Add the X Axis
     svg.append("g")
         .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 

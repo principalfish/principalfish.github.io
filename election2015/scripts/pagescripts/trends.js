045b53a22ecf448676c70a01371679fd53ffb525
principalfish <philipfharrison@gmail.com>
2015-03-11 12:53:25 +0000

    bump

--- election2015/scripts/pagescripts/trends.js
+++ election2015/scripts/pagescripts/trends.js
@@ -13,10 +13,10 @@ var y = d3.scale.linear().range([height, 0]);
 
 // Define the axes
 var xAxis = d3.svg.axis().scale(x)
-  .orient("bottom").ticks(5);
+  .orient("bottom").ticks(10);
 
 var yAxis = d3.svg.axis().scale(y)
-  .orient("left").ticks(5);
+  .orient("left").ticks(25);
 
 
 // Define the line

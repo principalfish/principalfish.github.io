e3588475c65f13555863ed93df4c45909777b826
principalfish <philipfharrison@gmail.com>
2015-03-12 12:40:51 +0000

    trends crap

--- election2015/scripts/pagescripts/trends.js
+++ election2015/scripts/pagescripts/trends.js
@@ -6,15 +6,18 @@ var currentState = "seats"
 
 var graphParties = ["conservative", "labour", "libdems", "ukip", "snp", "plaidcymru", "green"]
 
+var parseDate = d3.time.format("%Y-%m-%d").parse;
+
+
+
 function drawGraph(type){
 
   d3.selectAll("path")
        .remove();
   d3.selectAll("text")
         .remove();
-
-
-  var parseDate = d3.time.format("%Y-%m-%d").parse;
+  d3.selectAll("circle")
+        .remove();
 
   var x = d3.time.scale().range([0, width]);
   var y = d3.scale.linear().range([height, 0]);
@@ -32,13 +35,21 @@ function drawGraph(type){
       .y(function(d) { if (type == "seats") return y(d.seats); if (type =="percent") return y(d.percent)});
 
 
-
-
-
   d3.tsv("/election2015/data/trends.csv", function(error, data) {
 
+      var filteredData = [];
 
       data.forEach(function(d) {
+        if (graphParties.indexOf(d.party) != -1){
+          filteredData.push(d)
+        }
+
+
+
+      });
+
+
+      filteredData.forEach(function(d) {
           d.day = parseDate(d.day);
           if (type == "seats"){
             d.seats = +d.seats;
@@ -52,47 +63,74 @@ function drawGraph(type){
       });
 
       // Scale the range of the data
-      x.domain(d3.extent(data, function(d) { return d.day; }));
+      x.domain(d3.extent(filteredData, function(d) { return d.day; }));
 
-      var yMinimum = d3.min(data, function(d) {
+      var yMinimum = d3.min(filteredData, function(d) {
 
         if (type == "seats"){
-          return d.seats ;
+          value = d.seats - 20;
+          if (value < 0){
+            value = 0
           }
+          return value;
+        }
 
         if (type == "percent"){
-          return d.percent;
+          value = d.percent - 10;
+          if (value < 0){
+            value = 0
+          }
+          return value;
 
         }
       });
 
-      var yMaximum = d3.max(data, function(d) {
+      var yMaximum = d3.max(filteredData, function(d) {
         if (type == "seats")
-          return d.seats + 40;
+          return d.seats + 5;
         if (type == "percent")
-          return d.percent + 5; });
+          return d.percent + 2; });
 
       y.domain([yMinimum, yMaximum]);
 
 
       var dataNest = d3.nest()
           .key(function(d) {return d.party;})
-          .entries(data);
+          .entries(filteredData);
 
 
       dataNest.forEach(function(d) {
+        svg.append("path")
+          .attr("class", d.key)
+          .style("fill", "none")
+          .attr("d", line(d.values))
 
-        if (graphParties.indexOf(d.key) >= 0){
-          svg.append("path")
-
-            .attr("class", d.key)
-            .style("fill", "none")
-            .attr("d", line(d.values))
-            .on("mouseover", writeToTable(d.values))
-            .on("mouseout", emptyTable)
-
-          }
       });
+
+
+
+      filteredData.forEach(function(d) {
+
+
+          svg.selectAll("dot")
+              .data(filteredData)
+          .enter().append("circle")
+              .attr("r", 3)
+              .attr("cx", function(d) { return x(d.day); })
+              .attr("cy", function(d) {
+                if (type == "seats"){
+                  return y(d.seats);}
+                if (type == "percent"){
+                  return y(d.percent)
+                }
+                  })
+              .attr("class", function(d) {return d.party})
+              .on("mouseover", function(d) {return writeToTable(d, type, filteredData)})
+              .on("mouseout", emptyTable)
+
+        });
+
+
 
 
       svg.append("g")
@@ -148,15 +186,28 @@ function reDrawGraph(party){
 }
 
 
-function writeToTable(d) {
-  return function(){
-    console.log(d)
-    $("#graphinfo").empty();
-    $("#graphinfo").append("<h2>Date: " + "date" + "</h2>");
-    $.each(graphParties, function(i){
-      $("#graphinfo").append("<h3 class=\"" + graphParties[i] +"\">" + partylist[graphParties[i]] + ": number" + "</h3>")
+function writeToTable(d, type, filteredData) {
+
+    var data = []
+
+    date = d.day
+
+    $.each(filteredData, function(i){
+      if (filteredData[i].day == date){
+        
+      }
+
     });
-  }
+
+
+
+    $("#graphinfo").empty();
+    $("#graphinfo").append("<h2>Date: " + d.day + "</h2>");
+    $.each(graphParties, function(i){
+
+
+      $("#graphinfo").append("<h3 class=\"" + graphParties[i] +"\">" + partylist[graphParties[i]] + ": " + d.seats + "</h3>")
+    });
 
 
 }
@@ -164,5 +215,5 @@ function writeToTable(d) {
 function emptyTable(){
 
     $("#graphinfo").empty();
-  
+
 }

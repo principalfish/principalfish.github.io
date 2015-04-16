178140811a3856e50e3becd8c528f7a8ea7de37a
principalfish <philipfharrison@gmail.com>
2015-04-16 12:36:16 +0100

    test_election_update

--- election2015/live/autorun.py
+++ election2015/live/autorun.py
@@ -7,6 +7,7 @@ import subprocess
 while(True):
     print datetime.datetime.now()
     execfile("update_data.py")
+    time.sleep(5)
     subprocess.call("autorun.sh", shell = True)
 
-    time.sleep(118)
+    time.sleep(113)
--- election2015/live/autorun.sh
+++ election2015/live/autorun.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 git checkout master
 git add .
+git config --global push.default simple
 git commit -am test_election_update
 git push

58ab040ead433cfcbded97df21600e5d4cea9a40
principalfish <philipfharrison@gmail.com>
2015-04-16 12:55:01 +0100

    test_election_update

--- election2015/live/autorun.py
+++ election2015/live/autorun.py
@@ -9,7 +9,7 @@ while(True):
     print datetime.datetime.now()
     execfile("update_data.py")
     print "********************"
-    print "\n" * 5
+    print "\n" * 3
     time.sleep(5)
     subprocess.call("autorun.sh", shell = True)
     for i in range(113):

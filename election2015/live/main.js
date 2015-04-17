a85173b947d05663fec651551cb2b5698262378f
principalfish <philipfharrison@gmail.com>
2015-04-17 21:31:30 +0100

    full_election_update

--- election2015/live/main.js
+++ election2015/live/main.js
@@ -3,7 +3,7 @@
 /////ideas
 
 // NOT DONE
-// alter refresh delay rate based on time - for night itself - 11-12: 90s, 12-1 : 60 seconds, 1-4: 30s, 4-7: 60s, 7-: 120 seconds
+// alter refresh delay rate based on time - for night itself - 11-12: 90s, 12-1 : , 1-4: 30s, 4-7: 60s, 7-: 120 seconds
 // make css less shit
 
 

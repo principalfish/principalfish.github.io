0a5769b13e01e425a85222a9bb6f8140b044bbb4
principalfish <philipfharrison@gmail.com>
2015-04-18 12:59:18 +0100

    full_election_update

--- election2015/live/main.js
+++ election2015/live/main.js
@@ -1112,13 +1112,15 @@ function activateTicker(){
 			// }
 
 
-			$("#ticker").append("<tr class=\"tickerSeats "  + seatinfo.winning_party + "\" onclick=\"zoomToClickedFilteredSeat(seatInfoForTicker[" + i + "].geometry)\">" +
+			$("#ticker").append("<tr id=\"" +  + "\" class=\"tickerSeats "  + seatinfo.winning_party + "\" onclick=\"zoomToClickedFilteredSeat(seatInfoForTicker[" + i + "].geometry)\">" +
 													"<td style=\"padding-right: 8px\";>" + seatinfo.declared_at_simple + "</td ><td style=\"padding-right: 8px; width: 100%;\">"
 													+ seatinfo.name + "</td><td style=\"padding-right: 8px\">"
 													+ seatinfo.change.toUpperCase()	+ "</td></tr>")
 
 			if (currentSeats.indexOf(seatinfo.name) == -1){
 				currentSeats.push(seatinfo.name)
+
+
 			}
 
 		});

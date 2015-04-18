a8c15229c4a65aecffa5169cada392a4e1f5dddb
principalfish <philipfharrison@gmail.com>
2015-04-18 12:48:17 +0100

    full_election_update

--- election2015/live/main.js
+++ election2015/live/main.js
@@ -483,7 +483,7 @@ function possibleCoalitions(voteTotals){
 	});
 
 	$.each(coalitions, function(i){
-		$("#coalitionlist").append("<h3 class =\"coalitionlist\" style=\"\">" + coalitions[i].parties + ":" + coalitions[i].seats + "&nbsp&nbsp&nbsp</h3>")
+		$("#coalitionlist").append(coalitions[i].parties + ":" + coalitions[i].seats)
 	})
 
 

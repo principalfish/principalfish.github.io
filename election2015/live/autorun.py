29c274ccbb1ffbe2ac89e46e46d5c44e3a6daa2b
principalfish <philipfharrison@gmail.com>
2015-05-07 05:54:42 +0100

    spreadsheet test

--- election2015/live/autorun.py
+++ election2015/live/autorun.py
@@ -4,6 +4,7 @@ import subprocess
 import os
 import json
 import moment
+import operator
 
 seat_dict = open("seat_map.json").read()
 seat_map = json.loads(seat_dict)
@@ -14,7 +15,26 @@ old_data = json.loads(old)
 live_data = {}
 new_data = {}
 
+
+for_spreadsheet = []
+
 parties = ["conservative", "labour", "libdems", "ukip", "snp", "plaidcymru", "green", "uu", "sdlp", "dup", "sinnfein", "alliance", "other", "others"]
+
+party_map = {
+    "labour" : "Labour",
+    "conservative" : "Conservative",
+    "libdems" : "Lib Dem",
+    "ukip" : "UKIP",
+    "snp": "SNP",
+    "green" : "Green",
+    "plaidcymru" : "Plaid Cymru",
+    "uu" : "Ulster Unionists",
+    "dup" : "DUP",
+    "sinnfein" : "Sinn Fein",
+    "sdlp" : "SDLP",
+    "other" : "Other",
+    "others" : "Other"
+}
 
 seat_list = []
 
@@ -110,6 +130,38 @@ def get_data(file):
     if now - declared_at < 420:
         new_data[seat_name] = {"seat_info" : by_seat, "party_info" : by_party}
 
+
+    ## for spreadsheet update
+    # 2nd max old
+
+    old_votes_for_spreadsheet = {}
+
+    for party in parties:
+        ss_incumbent = old_data[seat_name]["incumbent"]
+        if party != ss_incumbent:
+            old_votes_for_spreadsheet[party] =  old_data[seat_name][party]
+
+    ss_second_large = max(old_votes_for_spreadsheet.iteritems(), key=operator.itemgetter(1))[0]
+
+    change_2010 = ""
+
+    if by_seat["change"] == "gain":
+        change_2010 = party_map[by_seat["winning_party"]] +  " gain from " + party_map[old_data[seat_name]["incumbent"]]
+
+    for_spreadsheet.append({
+        "No." : old_data[seat_name]["off_id"],
+        "Seat" : seat_name,
+        "Party" : party_map[old_data[seat_name]["incumbent"]],
+        "Vote 2010" : round(100 * old_data[seat_name][old_data[seat_name]["incumbent"]] / float(old_data[seat_name]["turnout"]), 2),
+        "Majority 2010" : old_data[seat_name]["majority_percent_2010"],
+        "2nd party 2010" : party_map[ss_second_large],
+        "2015 Expected Party" : "",
+        "2015 Actual Party" : party_map[by_seat["winning_party"]],
+        "Turnout 2015" : percentage_turnout,
+        "Change 2010" : change_2010
+
+        })
+
 seats_declared = 0
 
 while(True):
@@ -154,6 +206,12 @@ while(True):
             json.dump(new_data, out)
         print "updating git"
         #subprocess.call("autorun.sh", shell = True)
+
+        ## for spreadsheet update
+
+        print "updating spreadsheet json"
+        with open("spreadsheet_info.json", "w") as out_file:
+            json.dump(for_spreadsheet, out_file)
 
     print "\n" * 3
     seats_declared = total_seats

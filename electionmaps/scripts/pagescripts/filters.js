97999347f596ae73a612e18eb32f3bde567af616
principalfish <philipfharrison@gmail.com>
2015-05-18 17:46:46 +0100

    split scripts

---
+++ electionmaps/scripts/pagescripts/filters.js
@@ -0,0 +1,170 @@
+// when user selects region this removes old vote list and replaces with selection
+// problem with cache of tablesorter not being cleared result in multiple tables being displayed
+// just rewriting the html as fix
+
+function filterMap(){
+
+	d3.selectAll(".map")
+		.attr("opacity", 1)
+
+	$("#keyonmap").html("");
+
+
+	var	party = filterStates[0].party;
+	var gains = filterStates[1].gain;
+	var region = filterStates[2].region;
+	var majoritylow = filterStates[3].majoritylow
+	var majorityhigh = filterStates[4].majorityhigh
+
+	// change nonsense user inputs
+	if (isNaN(majoritylow))
+		majoritylow = 0
+	if (isNaN(majorityhigh))
+		majorityhigh = 100
+
+	// reset seatsAfterFilter from any previous filters.
+	seatsAfterFilter = [];
+
+	g.selectAll(".map")
+		.attr("id", "filtertime")
+
+	if (party == "null")
+		g.selectAll("#filtertime")
+			.attr("id", "partyfiltered")
+	else
+		g.selectAll("#filtertime")
+			.attr("style", function(d){
+				if (party != seatData[d.properties.name]["seat_info"]["winning_party"])
+					return "opacity: 0.1";
+				})
+			.attr("id", function(d){
+				if (party == seatData[d.properties.name]["seat_info"]["winning_party"])
+					return "partyfiltered"
+				});
+
+	if (gains == "null")
+			g.selectAll("#partyfiltered")
+				.attr("id", "gainfiltered")
+
+	else
+		if (gains == "gains")
+			g.selectAll("#partyfiltered")
+				.attr("style", function(d) {
+					if (seatData[d.properties.name]["seat_info"]["change"] == "hold")
+						return "opacity: 0.1";
+					})
+				.attr("id", function(d){
+					if (seatData[d.properties.name]["seat_info"]["change"] == "gain")
+						return "gainfiltered"
+				});
+
+		if (gains == "nochange")
+			g.selectAll("#partyfiltered")
+				.attr("style", function(d) {
+					if (seatData[d.properties.name]["party"] != "gain")
+						return "opacity: 0.1";
+					})
+				.attr("id", function(d){
+					if (seatData[d.properties.name]["seat_info"]["change"] == "hold")
+						return "gainfiltered"
+				});
+
+	if (region == "null")
+		g.selectAll("#gainfiltered")
+			.attr("id", "regionfiltered")
+
+	else
+		g.selectAll("#gainfiltered")
+			.attr("style", function(d){
+				if (region != seatData[d.properties.name]["seat_info"]["area"])
+					return "opacity: 0.1";
+			})
+			.attr("id", function(d){
+				if (region == seatData[d.properties.name]["seat_info"]["area"])
+					return "regionfiltered"
+			});
+
+	g.selectAll("#regionfiltered")
+		.attr("style", function(d) {
+
+			if (parseFloat(seatData[d.properties.name]["seat_info"]["maj_percent"]) < majoritylow || parseFloat(seatData[d.properties.name]["seat_info"]["maj_percent"]) > majorityhigh )
+				return "opacity: 0.1"
+
+			})
+		.each(function(d) {
+			if (parseFloat(seatData[d.properties.name]["seat_info"]["maj_percent"]) >= majoritylow && parseFloat(seatData[d.properties.name]["seat_info"]["maj_percent"]) <= majorityhigh )
+				seatsAfterFilter.push(d);
+			});
+
+	g.selectAll(".map")
+		.attr("id", function(d) {
+			return "i" + d.properties.info_id
+		});
+
+
+	generateSeatList();
+	$(seatTotalContainer).html(" ");
+	$(seatListContainer).html(" ");
+
+	}
+
+// resets all filters and map to default state
+function resetFilter(){
+
+	filterStates[0].party = "null";
+	filterStates[1].gain = "null";
+	filterStates[2].region = "null";
+	filterStates[3].majoritylow = 0;
+	filterStates[4].majorityhigh = 100;
+
+	var previous_opacity = 1;
+	var current_colour = 1;
+
+	swingState = ["null", "null"];
+
+	partyVoteShare = "null"
+	partyVoteShareChange = "null"
+
+	filterMap();
+
+	$("#votesharebypartyselect option:eq(0)").prop("selected", true);
+	$("#votesharechangebypartyselect option:eq(0)").prop("selected", true);
+	$("#swingfrom option:eq(0)").prop("selected", true);
+	$("#swingto option:eq(0)").prop("selected", true);
+
+
+	$("#dropdownparty option:eq(0)").prop("selected", true);
+	$("#dropdowngains option:eq(0)").prop("selected", true);
+	$("#dropdownregion option:eq(0)").prop("selected", true);
+	$("#majority").get(0).reset();
+
+	$(seatTotalContainer).html(" ");
+	$(seatListContainer).html(" ");
+}
+
+// using seatsAfterFilter, generates list of filtered seats
+function generateSeatList(){
+			$("#totalfilteredseats").html(" ");g
+			$("#filteredlisttable").html(" ");
+
+			$(seatTotalContainer).html(" ");
+			$(seatListContainer).html(" ");
+
+			$(seatTotalContainer).append("<p>Total : " + seatsAfterFilter.length + "</p>");
+			$.each(seatsAfterFilter, function(i){
+
+			if (filterStates[1].gain == "gains" && filterStates[0].party != "null"){
+							$(seatListContainer).append("<div onclick=\"zoomToClickedFilteredSeat(seatsAfterFilter[" + i + "])\">"
+              + "<div class=\"party-flair " + seatData[seatsAfterFilter[i].properties.name]["seat_info"]["incumbent"] + "\"></div>"
+              + seatsAfterFilter[i].properties.name
+              + "</div>")
+				}
+
+			else{
+          $(seatListContainer).append("<div onclick=\"zoomToClickedFilteredSeat(seatsAfterFilter[" + i + "])\">"
+          + '<div class=\"party-flair ' + seatData[seatsAfterFilter[i].properties.name]["seat_info"]["winning_party"] + '\"></div>'
+          + seatsAfterFilter[i].properties.name
+          + "</div>");
+				}
+		});
+	}

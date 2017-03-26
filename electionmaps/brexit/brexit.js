5e760b9bf48d91657a78ddfb8d44340ab62011e9
principalfish <philipfharrison@gmail.com>
2017-03-26 11:48:40 +0100

    filters

--- electionmaps/brexit/brexit.js
+++ electionmaps/brexit/brexit.js
@@ -97,7 +97,109 @@ var regionMap = {
 
 };
 ;var filters = {
-  opacities : {}
+  state : {
+    "leave" : [0, 100]
+  },
+
+  selectHandle : function(parameter, criteria){
+
+    parameter = parameter.substring(7); // trim filter- off of parameters
+
+
+    if (criteria == "null"){
+      delete filters.state[parameter]
+    } else {
+      if (parameter == "region" && criteria == "england"){
+        filters.state["region"] = regionMap["england"]
+      } else {
+        filters.state[parameter] = criteria;
+      }
+    }
+
+    filters.filter();
+  },
+
+
+
+  leave : function(parameter, value){
+    if (parameter == "low"){
+      filters.state.leave[0] = value;
+    } else if (parameter == "high"){
+      filters.state.leave[1] = value;
+    }
+    filters.filter()
+  },
+
+  opacities : {},
+
+  filter : function(){
+
+    $.each(currentMap.seatData, function(seat, data){
+      var meetsCriteria = true
+
+
+      $.each(filters.state, function(parameter, criteria){
+
+        if (parameter == "leave"){
+          var leave = data.leave;
+
+          if (leave < filters.state["leave"][0] || leave > filters.state["leave"][1] ){
+            meetsCriteria = false;
+          }
+
+        }  else if (parameter == "current") {
+          if (data.current != criteria){
+            meetsCriteria = false;
+          }
+
+        } else {
+
+          //if (data.seatInfo[parameter] != criteria){ // TO DO CHECK NOT BROKEN
+          if (criteria.indexOf(data[parameter]) == - 1){
+            meetsCriteria = false;
+          }
+        }
+      });
+
+      if (meetsCriteria == true){
+        var current = currentMap.seatData[seat]["current"];
+        filters.opacities[seat] = 10 * ((currentMap.seatData[seat][current] / 100) - 0.5) ;
+        data.filtered = true;
+      } else {
+        filters.opacities[seat] = 0.00;
+        data.filtered = false;
+      }
+    })
+
+    filters.display();
+    //filters.getSeatlist();
+  },
+
+  display: function(){
+    $.each(filters.opacities, function(seat, opacity){
+
+      var mapSelect = currentMap.seatData[seat].mapSelect;
+      mapSelect.opacity = opacity;
+      d3.select("#i" + mapSelect.properties.info_id).attr("opacity", opacity)
+    });
+  },
+
+  reset : function(){
+    filters.state = {"leave" : [0, 100]}; // reset filters state
+    // reset ui
+    $("#filter-current option:eq(0)").prop("selected", true);
+    $("#filter-winner2015 option:eq(0)").prop("selected", true);
+  	$("#filter-region option:eq(0)").prop("selected", true);
+  	$("#filter-leave").get(0).reset();
+
+    // reset map
+    filters.filter();
+  },
+
+  filteredList : []
+
+
+
 }
 ;var mapAttr = {
   "width" : 600,
@@ -570,6 +672,7 @@ function initialization(){
 
   // map buttons to divs
   buttonToDiv : {
+    "brexitvotesbutton" : "#brexitvotes",
     "seat-information" : "#seat-information",
     "votetotalsbutton" : "#votetotals",
     "filtersbutton" : "#filters",
@@ -607,7 +710,7 @@ function initialization(){
 
   pageLoadDiv : function(){
     $.each(uiAttr.buttonToDiv, function(button, div){
-      if (button == "votetotalsbutton" || button == "filtersbutton" ){
+      if (button == "brexitvotesbutton" || button == "filtersbutton" ){
         uiAttr.showDiv(button);
       } else if (button == "predictbutton" && currentMap.predict == true){
         $("#predictbutton").removeClass("hidden").addClass("mapbuttonactive");

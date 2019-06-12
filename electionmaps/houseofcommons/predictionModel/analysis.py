c255a442570a16dd4e22912afffdf57bf2d8e0bf
LP <philipfharrison@gmail.com>
2019-06-12 18:14:05 +0100

    jun 12 poll

--- electionmaps/houseofcommons/predictionModel/analysis.py
+++ electionmaps/houseofcommons/predictionModel/analysis.py
@@ -34,12 +34,12 @@ for region in regions:
 polling_data = "polls.csv"
 
 with open(polling_data, "rb") as polls_file:
-    poll_data = csv.DictReader(polls_file, delimiter = ",")
+    poll_data = csv.DictReader(polls_file, delimiter = "\t")
     poll_codes = []
     poll_rows = []
 
     for row in poll_data:
-        print row
+
         code = row["code"]
         if code not in poll_codes:
 

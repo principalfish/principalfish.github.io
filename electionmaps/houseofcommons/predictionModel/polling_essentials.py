7a267d923de5fe30aeea438deb719ad8270d3b9b
LP <philipfharrison@gmail.com>
2020-04-09 08:45:40 +0100

    fix weighting

--- electionmaps/houseofcommons/predictionModel/polling_essentials.py
+++ electionmaps/houseofcommons/predictionModel/polling_essentials.py
@@ -319,7 +319,7 @@ class Poll(object):
         weight = pollster_weights[self.company]
 
         # alter closer to election  when more polls
-        degrade_factor = 0.25 #per day
+        degrade_factor = 0.85 #per day
         weight *= math.pow(degrade_factor, days_past)
 
         #testing

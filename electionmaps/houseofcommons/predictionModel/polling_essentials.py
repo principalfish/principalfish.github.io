5379dd85766c0166f12ab5af1a2e36ce805fd95f
LP <philipfharrison@gmail.com>
2020-12-01 20:23:52 +0000

    dec 1 polls

--- electionmaps/houseofcommons/predictionModel/polling_essentials.py
+++ electionmaps/houseofcommons/predictionModel/polling_essentials.py
@@ -321,7 +321,7 @@ class Poll(object):
         weight = pollster_weights[self.company]
 
         # alter closer to election  when more polls
-        degrade_factor = 0.9 #per day
+        degrade_factor = 0.92 #per day
         weight *= math.pow(degrade_factor, days_past)
 
         #testing

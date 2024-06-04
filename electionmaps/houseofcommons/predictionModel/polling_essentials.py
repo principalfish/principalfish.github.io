66c8444008c914f966d797db14a2dc4091f80e67
principalfish <principalfish@gmail.com>
2024-06-04 20:37:15 +0100

    jun 4 polls

--- electionmaps/houseofcommons/predictionModel/polling_essentials.py
+++ electionmaps/houseofcommons/predictionModel/polling_essentials.py
@@ -324,7 +324,7 @@ class Poll(object):
         weight = pollster_weights[self.company]
 
         # alter closer to election  when more polls
-        degrade_factor = 0.75 #per day
+        degrade_factor = 0.7 #per day
         weight *= math.pow(degrade_factor, days_past)
 #
         #testing
